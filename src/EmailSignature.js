3c157b2e6d194c91320260b51faf9aaddd366171
Andrew <andrew.mecham@gmail.com>
2016-12-04 20:38:22 -0700

    created header component and styles

--- src/EmailSignature.js
+++ src/EmailSignature.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import { render } from 'react-dom'
+import Header from './Header'
 import '../public/styles/reset.css'
 import '../public/styles/style.css'
 
 const App = React.createClass({
   render () {
     return (
-      <div className='app'>hello</div>
+      <div className='app'>
+        <Header />
+        <div className='container'>
+          <div className='step-one'></div>
+          <div className='step-two'></div>
+        </div>
+      </div>
     )
   }
 })
---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = React.createClass({
+  render () {
+    return (
+      <header className='header-box'>
+        <h1 className='header-title'>
+          Andrew Mecham Technologies
+        </h1>
+      </header>
+    )
+  }
+})
+
+export default Header
